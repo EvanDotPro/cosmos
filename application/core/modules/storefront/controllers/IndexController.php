89ed63e2ea0379f4ce2461ce2ca4857b01b44b18
Evan Coury <me@evancoury.com>
2010-03-31 11:23:56 -0700

    implemented translations full force now

--- application/core/modules/storefront/controllers/IndexController.php
+++ application/core/modules/storefront/controllers/IndexController.php
@@ -4,6 +4,7 @@ class Storefront_IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         Cosmos_Profiler::start('calc.multiply');
+        // This is not typical because a core action would not be using add-on methods.
         $this->view->result = Cosmos_Api::get()->calc->divide(10,2);
         $this->view->service_plugin_result = Cosmos_Api::get()->sample->add(5,5);
         Cosmos_Profiler::stop('calc.multiply');
